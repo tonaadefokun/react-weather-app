a137e7812421419a2c8693ae0a56f2a710e6f797
tonaadefokun <84527335+tonaadefokun@users.noreply.github.com>
2021-09-17 15:26:49 +0100

    Add the GitHub repository URL.

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <p>
+        <footer>
+          The project was coded by Adefokun Adetona and it's open-sourced on:{" "}
+          <a
+            href="https://github.com/tonaadefokun/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </p>
+    </div>
+  );
 }
