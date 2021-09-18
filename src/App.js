3854d465d081ba157dd4ca5253acfac367c6d6f7
tonaadefokun <84527335+tonaadefokun@users.noreply.github.com>
2021-09-18 11:46:29 +0100

    Add  bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <p>
-        <footer>
-          The project was coded by Adefokun Adetona and it's open-sourced on:{" "}
-          <a
-            href="https://github.com/tonaadefokun/weather-app-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>
-        </footer>
-      </p>
+      <div className="container">
+        <h1> Weather App</h1>
+        <p>
+          <footer>
+            The project was coded by Adefokun Adetona and it's open-sourced on:{" "}
+            <a
+              href="https://github.com/tonaadefokun/weather-app-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+            <p />
+            <button className="btn btn-primary">Button</button>
+          </footer>
+        </p>
+      </div>
     </div>
   );
 }
